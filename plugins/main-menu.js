236f603cf0d807bf96f1635f0004814065341eb5
nainzBot <121393944+nainzBot@users.noreply.github.com>
2023-01-01 08:43:01 +0700

    kizubot
    kizubot

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -258,7 +258,7 @@ let fot = `𝙽𝚘𝚝𝚎 𝙹𝚒𝚔𝚊 𝙼𝚎𝚗𝚎𝚖𝚞𝚔𝚊�
 `
 const listMessage = {
 footer: fot,
-text: 'Made By Jarot Offc',
+text: 'Made By NainOfc',
 mentions: await conn.parseMention(jarot),
 title: jarot,
 buttonText: `CLICK HERE ⎙`, 
