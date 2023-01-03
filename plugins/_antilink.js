2cec66867ded26e9a6e6c085b72a9e97cf546b30
nainzBot <121393944+nainzBot@users.noreply.github.com>
2023-01-03 15:49:05 +0700

    yy
    yy

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,7 +7,7 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
+    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* waduh ada yang ngirim link gc lain\n\padahal kan antilink nya sudah nyala, dan gaboleh kirim link group lain, maaf:)`)
     if (isAdmin) return m.reply('*Eh sorry you admins, you will not be kicked. hehe..*')
     if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people _-*')
     let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
